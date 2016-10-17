76c73b5a0d6382e69fbb08a29c53e715ea3fe48d
davis854 <davis854@marshall.edu>
2016-10-17 01:56:55 -0400

    Removed the HandleErrorAttritube exception

--- TicketingSystem/App_Start/FilterConfig.cs
+++ TicketingSystem/App_Start/FilterConfig.cs
@@ -7,7 +7,8 @@ namespace TicketingSystem
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
+            
         }
     }
 }
